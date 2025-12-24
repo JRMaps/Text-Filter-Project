0721d6a682903ef8ec1c8d8fde871b86b9f7a959
martbungque <raymartbungque18@gmail.com>
2025-12-24 21:35:40 +0800

    try lang: added mock database, routes, controllers based on the mock db

---
+++ backend/app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from backend.app.api.routes import auth_routes, message_routes
+
+app = FastAPI()
+
+# test
+@app.get("/")
+async def get_app():
+    return {"message": "Hello, World!"}
+
+# Include routers
+app.include_router(auth_routes.router, prefix="/auth", tags=["auth"])
+app.include_router(message_routes.router, prefix="/messages", tags=["messages"])
